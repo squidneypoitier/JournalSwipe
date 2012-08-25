a01984bddff0daa74202e20000bd552b197096d4
GlocktopusPrime <glocktopusprime@gmail.com>
2012-08-24 22:20:11 -0700

    Restructured the packages in the project, changed how exceptions are handled.
    Signed-off-by: GlocktopusPrime <glocktopusprime@gmail.com>

--- make_pdf.py
+++
@@ -1,8 +0,0 @@
-"""
-Library for taking individual PDFs and building them into a single PDF, 
-checking for duplicate pages and trying to assemble them in the right order.
-
-@author: GlocktopusPrime
-@version: 0.1
-
-"""
